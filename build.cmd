f47875a1b26c194a3336484604f184a0fb384bb6
Pierre Chalamet <pierre@chalamet.net>
2016-12-26 22:47:19 +0100

    initial commit

---
+++ build.cmd
@@ -0,0 +1,38 @@
+set HERE=%~dp0
+set PAKETBOOTSTRAP=%HERE%tools/paket.bootstrapper.exe
+set PAKET=%HERE%tools/paket.exe
+set FULLBUILD=%HERE%packages/full-build/tools/fullbuild.exe
+
+%PAKETBOOTSTRAP%
+
+set QAFOLDER=%HERE%qa-cassandrasharp
+%PAKET% restore || goto :ko
+%FULLBUILD% setup git https://github.com/pchalamet/cassandra-sharp-full-build %HERE%binrepo %QAFOLDER% || goto :ko
+cd %QAFOLDER% || goto :ko
+
+%FULLBUILD% add nuget https://www.nuget.org/api/v2/ || goto :ko
+%FULLBUILD% add repo cassandra-sharp https://github.com/pchalamet/cassandra-sharp || goto :ko
+%FULLBUILD% add repo cassandra-sharp-contrib https://github.com/pchalamet/cassandra-sharp-contrib || goto :ko
+%FULLBUILD% clone *  || goto :ko
+%FULLBUILD% index * || goto :ko
+%FULLBUILD% convert * || goto :ko
+%FULLBUILD% view all * || goto :ko
+%FULLBUILD% view csc cassandra-sharp-contrib/* || goto :ko
+%FULLBUILD% list view || goto :ko
+%FULLBUILD% describe view all || goto :ko
+%FULLBUILD% graph all || goto :ko
+%FULLBUILD% build all || goto :ko
+%FULLBUILD% history || goto :ko
+%FULLBUILD% drop view csc || goto :ko
+%FULLBUILD% outdated package || goto :ko
+%FULLBUILD% update package || goto :ko
+%FULLBUILD% push 42 || goto :ko
+
+:ok
+echo *** SUCCESSFUL
+exit /b 0
+
+:ko
+cd %HERE%
+echo *** FAILURE
+exit /b 0
